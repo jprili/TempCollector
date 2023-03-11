2d167708420c83c2d91405136c713fc5f9595a32
jprili <rilijames@gmail.com>
2023-03-11 13:26:10 -0800

    added point #; refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@
 #define BAUD_RATE 9600
 
 dht sensor;
-int status = 1; // placeholder status
 bool runOnce = 1;
+uint32_t loopCounter = 0;
+int status = INT8_MIN;
 
 void setup() {
   // serial setup
@@ -15,34 +16,42 @@ void setup() {
   Serial.println(BAUD_RATE);
 
   // initial status check for DHT11
-  int status = sensor.read11(DHT_PIN);
+  status = sensor.read11(DHT_PIN);
+
   if (status == DHTLIB_OK) {
     Serial.println("[INFO] Connection OK.\n"
                     "Moving to loop.");
+
   } else {
     Serial.print("[WARN] Connection Status ");
     Serial.print(status);
     Serial.println(". Please check sensor connection.");
+
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if (status == DHTLIB_OK) {
+
     if (runOnce) {
-      Serial.println("Humidity (%), \t Temperature: (degC)");
+      Serial.println("Point #,\tHumidity (%),\tTemperature: (degC)");
     }
     runOnce = 0;
     
     // collect humidity and temperature
     // round to 1 decimal
+    Serial.print(loopCounter);
+    Serial.print(",\t");
     Serial.print(sensor.humidity, 1);
-    Serial.print(", \t");
-    Serial.print(sensor.temperature, 1);
-    Serial.println(" ");
+    Serial.print(",\t");
+    Serial.println(sensor.temperature, 1);
+    loopCounter++;
   } else {
+    Serial.println(status);
     delay(10000);
   }
-
+  
+  delay(2000);
   status = sensor.read11(DHT_PIN);
 }
