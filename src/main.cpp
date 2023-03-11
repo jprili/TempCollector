deea23ca4097860290d016b559ae77fc280a2f5d
jprili <rilijames@gmail.com>
2023-03-11 10:19:28 -0800

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <dht.h>
+
+#define DHT_PIN 2
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
